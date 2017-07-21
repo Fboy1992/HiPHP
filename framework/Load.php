193800a9b5e5828a84573db0e3111b9926d61692
aizsfgk <1732756017@qq.com>
2017-07-22 00:10:02 +0800

    add autoload_register function

--- framework/Load.php
+++ framework/Load.php
@@ -2,7 +2,46 @@
 /**
  * 自动加载类
  */
+namespace framework;
+
+use framework\App;
+use Exception;
+
 class Load
 {
+	// 类名映射
+	public static $map = [];
+	// 类名空间映射
+	public static $namespaceMap = [];
 
+	// 应用启动注册
+	public static function register(App $app)
+	{
+		self::$namespaceMap = [
+			'framework' => $app->rootPath
+		];
+
+		spl_autoload_register(['framework\Load', 'autoload']);
+
+	}
+
+	private static function autoload($class)
+	{
+		$classOrigin = $class;
+		$classInfo = explode('\\', $class);
+		$className = array_pop($classInfo);
+
+		foreach ($classInfo as &$v) {
+			$v = strtolower($v);
+		}
+		array_push($classInfo, $className);
+		$class = implode('\\', $classInfo);
+		$path = self::$namespaceMap['framework'];
+		$classPath = $path . '/' . str_replace('\\', '/', $class) . '.php';
+		
+		// composer
+
+		self::$map[$classOrigin] = $classPath;
+		require $classPath;
+	}
 }
