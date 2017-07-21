253d2baf4ec88add4ede709b069de906092bec26
aizsfgk <1732756017@qq.com>
2017-07-21 20:05:14 +0800

    coding in english

--- framework/exception/ExceptionCodes.php
+++ framework/exception/ExceptionCodes.php
@@ -7,10 +7,10 @@ namespace framework;
 class ExceptionCodes
 {	
 	// -1 ~ -10 基本异常
-	public static $baseEcp = ['msg'=>'基本异常', 'code'=>-1];
-	// -11 ~ 100 框架异常
-	public static $setEcp = ['msg'=>'set异常', 'code'=>-11];
-	public static $getEcp = ['msg'=>'get异常', 'code'=>-12];
-	public static $setSingleEcp = ['msg'=>'setSingle异常', 'code'=>-13];
-	public static $getSingleEcp = ['msg'=>'getSingle异常', 'code'=>-14];
+	public static $baseEcp = ['msg'=>'base_exception', 'code'=>-1];
+	// -11 ~ -100 框架异常
+	public static $setEcp = ['msg'=>'set_exception', 'code'=>-11];
+	public static $getEcp = ['msg'=>'get_exception', 'code'=>-12];
+	public static $setSingleEcp = ['msg'=>'setSingle_exception', 'code'=>-13];
+	public static $getSingleEcp = ['msg'=>'getSingle_exception', 'code'=>-14];
 }
