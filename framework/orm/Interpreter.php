dfe9495baf4dda1c902d4304e7d594a516831269
aizsfgk <1732756017@qq.com>
2017-07-25 14:59:57 +0800

    add db sql interpreter using trait

--- framework/orm/Interpreter.php
+++ framework/orm/Interpreter.php
@@ -5,7 +5,79 @@
  */
 namespace framework\orm;
 
+use framework\exception\CoreHttpException;
+
 trait Interpreter
 {
+	private $where = '';
+	public $params = '';
+	private $orderBy = '';
+	private $limit = '';
+	private $offset = '';
+	private $sql = '';
 
+	// 插入一条数据
+	public function insert($data = [])
+	{
+		if (empty($data)) {
+			throw new CoreHttpException(400, "argument data is null", 400);
+		}
+
+		$fieldString = '';
+		$valueString = '';
+		$i = 0;
+		foreach ($data as $k => $v) {
+			if ($i === 0) {
+				$fieldString .= "`{$k}`";
+				$valueString .= ":{$k}";
+				$this->params[$k] = $v;
+				++$i;
+				continue;
+			}
+
+			$fieldString .= " , `{$k}`";
+			$valueString .= " , :{$k}";
+			$this->params[$k] = $v;
+			++$i;
+		}
+		unset($k);
+		unset($v);
+
+		$this->sql = "INSERT INTO `{$this->tableName}` ({$fieldString}) VALUES ($valueString)";
+	}
+
+	// 删除
+	public function del($data = [])
+	{
+		$this->sql = "DELETE FROM `{$this->tableName}`";
+	}
+
+	// 跟新数据
+	// 
+	public function updateData($data = [])
+	{
+		if (empty($data)) {
+			throw new CoreHttpException(400, "argument data is null");
+		}
+		$set = '';
+		$dataCopy = $data;
+		$pop = array_pop($dataCopy);
+		foreach ($$data as $k => $v) {
+			if ($v === $pop) {
+				$set .= "`{$k}` = :$k";
+				$this->params[$k] = $v;
+				continue;
+			}
+		}
+		$this->sql = "UPDATE `{$this->tableName}` SET {$set}";
+	}
+
+	// 查找一条数据
+	
+	// where 语句
+	// 
+	public function where($data = array())
+	{
+		
+	}
 }
--- framework/orm/Model.php
+++ framework/orm/Model.php
@@ -18,6 +18,45 @@ class Model extends DB
 
 	public function getTableName()
 	{
-		
+		$prefix = App::$container->getSingle('config')
+		                         ->config['database']['dbprefix'];
+		// get_called_class 返回后期静态绑定的类的名称
+		$callClassName = get_called_class();
+		$callClassName = explode('\\', $callClassName);
+		$callClassName = array_pop($callClassName);
+
+		if (!empty($this->tableName)) {
+			if (empty($prefix)) {
+				return;
+			}
+			$this->tableName = $prefix . '_' . $this->tableName;
+			return;
+		}
+
+		preg_match_all('/([A-Z][a-z]*)/', $callClassName, $match);
+		if (!isset($match[1][0]) || empty($match[1][0])) {
+			throw new CoreHttpException(401, 'model name invalid');			
+		}
+		$match = $match[1];
+		$count = count($match);
+
+		if ($count === 1) {
+			$this->tableName = strtolower($match[0]);
+			if (empty($prefix)) {
+				return;
+			}
+			$this->tableName = $prefix . '_' . $this->tableName;
+			return;
+		}
+		$last = strtolower(array_pop($match));
+		foreach ($match as $v) {
+			$this->tableName .= strtolower($v) . '_';
+		}
+
+		$this->tableName .= $last;
+		if (empty($prefix)) {
+			return;
+		}
+		$this->tableName = $prefix . '_' . $this->tableName;
 	}
 }
